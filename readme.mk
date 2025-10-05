7b89862d2982a61e43f960faaf9f90f556557130
Gerard <alu.gerard.pedrosa@mataro.epiaedu.cat>
2025-10-05 20:03:01 +0200

    Creació del projecte web personal

---
+++ readme.mk
@@ -0,0 +1,4 @@
+# Pàgina Personal de Gerard
+
+Aquesta és una activitat pràctica per aprendre a crear una pàgina web personal senzilla utilitzant HTML, CSS i Git.  
+L’objectiu és practicar el procés complet de desenvolupament: des de la creació del projecte local, els commits, fins a la pujada a GitHub.
